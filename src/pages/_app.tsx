cce301579d493f8caf04e39720e614a4523ac7bf
carla <carla@mail.com>
2023-03-28 22:44:55 +0100

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://sandbox.web.squarecdn.com/v1/square.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
